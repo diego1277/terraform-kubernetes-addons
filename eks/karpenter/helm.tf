00ff7c6960759bc4d5457c8191e03349bb25fed4
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-11-30 16:25:07 -0300

    Create helm.tf

---
+++ eks/karpenter/helm.tf
@@ -0,0 +1,25 @@
+module "helm" {
+  source = "github.com/diego1277/terraform-kubernetes-addons.git//helm"
+  name = "aws-ebs-csi-driver"
+  namespace = "karpenter"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart = "aws-ebs-csi-driver"
+  custom_values = {
+    image_repo = {
+      name = "image.repository"
+      value = "${local.image_registry}/eks/aws-ebs-csi-driver"
+    }
+    sa_create = {
+      name = "controller.serviceAccount.create"
+      value = "true"
+    }
+    sa_name = {
+      name = "controller.serviceAccount.name"
+      value = var.service_account_name
+    }
+    sa_annotation = {
+      name = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+      value = module.irsa.role_arn
+    }
+  }
+}
