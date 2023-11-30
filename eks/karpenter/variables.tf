25da01fb68eb95092c77231f3ea9bf7221598ed9
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-11-30 16:19:07 -0300

    Create variables.tf

---
+++ eks/karpenter/variables.tf
@@ -0,0 +1,14 @@
+variable cluster_name {
+  description = "cluster name"
+  type        = string
+}
+
+variable cluster_openid_connect_url {
+  description = "cluster openid connect url"
+  type        = string
+}
+
+variable cluster_openid_connect_arn {
+  description = "cluster openid connect url"
+  type        = string
+}
