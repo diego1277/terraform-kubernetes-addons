73e59d36fa84ecbf9597e5b9f1c9ce43c5a7cc80
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-11-30 16:32:29 -0300

    Create helm.tf

---
+++ eks/karpenter/helm.tf/helm.tf
@@ -0,0 +1,30 @@
+resource "helm_release" "karpenter" {
+    namespace        = "karpenter"
+    create_namespace = true
+
+    name       = "karpenter"
+    repository = "https://charts.karpenter.sh"
+    chart      = "karpenter"
+    version    = "v0.13.1"
+
+    set {
+        name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+        value = aws_iam_role.karpenter_role.arn
+    }
+
+    set {
+        name  = "clusterName"
+        value = var.cluster_name
+    }
+
+    set {
+        name  = "clusterEndpoint"
+        value = var.cluster_endpoint
+    }
+/*
+    set {
+        name  = "aws.defaultInstanceProfile"
+        value = aws_iam_instance_profile.nodes.name
+    }
+*/
+}
