e05ad9465466598916f7dead4962895fc7035761
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-12-08 19:13:41 -0300

    Update helm.tf

--- eks/karpenter/helm.tf
+++ eks/karpenter/helm.tf
@@ -3,7 +3,7 @@ resource "helm_release" "karpenter" {
     create_namespace = true
 
     name       = "karpenter"
-    repository = "https://charts.karpenter.sh"
+    repository = "oci://public.ecr.aws/karpenter/karpenter"
     chart      = "karpenter"
     version    = var.chart_version
 
