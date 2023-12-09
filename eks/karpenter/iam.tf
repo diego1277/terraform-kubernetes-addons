0d6e910ad8ab5447b5cd27114475e74e2afd1183
diego1277 <59052924+diego1277@users.noreply.github.com>
2023-12-09 11:20:50 -0300

    Update iam.tf

--- eks/karpenter/iam.tf
+++ eks/karpenter/iam.tf
@@ -43,6 +43,7 @@ data "aws_iam_policy_document" "karpenter_policy" {
             "ec2:DescribeAvailabilityZones",
             "ec2:DescribeSpotPriceHistory",
             "ec2:RunInstances",
+            "ec2:DescribeImages",
             "ssm:GetParameter",
             "iam:PassRole",
             "pricing:GetProducts"
